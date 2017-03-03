329dd0bcc482999c5f87131a6784df3768348997
qwer777 <dontspammebr4h@gmail.com>
2017-03-03 17:34:26 -0600

    fix setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+shopt -s nullglob dotglob     #To include hidden files
 scriptfolder="$(dirname "${BASH_SOURCE}")"
 while read line;
   do
@@ -6,6 +7,7 @@ while read line;
     GHREPO=$(echo $line | tr "/" " " | awk '{print $2}')
     if [ -e "$HOME/github.com/$GHUSER/$GHREPO" ]
     then 
+      echo moving "$line" to "$line.$(date +%F_%T)"
       mv "$HOME/github.com/$GHUSER/$GHREPO" "$HOME/github.com/$GHUSER/$GHREPO.$(date +%F_%T)"
     fi
     mkdir -p "$HOME/github.com/$GHUSER" && cd "$HOME/github.com/$GHUSER" && git clone https://github.com/$GHUSER/$GHREPO.git
@@ -15,7 +17,9 @@ for link in $scriptfolder/symlink/*
   do
     basefile="$(basename "$link")"
     if [ -e "$HOME/$basefile" ]
-      mv "$HOME/$basefile" "$HOME/$basefile.$(date +%F_%T)"
-    fi
+      then
+        echo moving "$basefile" to "$basefile.$(date +%F_%T)"
+        mv "$HOME/$basefile" "$HOME/$basefile.$(date +%F_%T)"
+      fi
     cp -d "$link" "$HOME/$basefile"
   done
