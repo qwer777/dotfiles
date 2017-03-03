378f921af27a726b68345c4f4cb700a89d764fb8
qwer777 <fearofunknown@gmail.com>
2017-03-03 13:47:05 -0600

    Create setup.sh

---
+++ setup/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+GitRepoArray=('qwer777/dotfiles','zsh-users/zsh-syntax-highlighting','zsh-users/zsh-history-substring-search','robbyrussell/oh-my-zsh')
+for i in "${GitRepoArray[@]}"
+do
+   GHUSER=$(echo $i | tr "/" " " | awk '{print $1}')
+   GHREPO=$(echo $i | tr "/" " " | awk '{print $2}')
+   if [ ! -d "$HOME/github.com/$GHUSER/$GHREPO ]
+   then 
+      mkdir -p "$HOME/github.com/$GHUSER" && cd "$HOME/github.com/$GHUSER" && git clone https://github.com/$GHUSER/$GHREPO.git
+   else
+      echo $i already exists
+done
