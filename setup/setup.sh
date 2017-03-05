615dd751974b764aee68df35387caa289efc10c0
qwer777 <dontspammebr4h@gmail.com>
2017-03-05 17:58:58 -0600

    major fixes to setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,45 +1,59 @@
 #!/bin/bash
 shopt -s nullglob dotglob     #To include hidden files
 ##Variables
-GITDIR="$HOME/github.com"
 #In case anyone wants to fork it, you just have to change the USER and REPO here
-THISUSER=qwer777
-THISREPO=dotfiles
-scriptfolder="$GITDIR/$THISUSER/$THISREPO/setup"
-
-#add the repo if it hasn't been already
-if [ ! -d "$GITDIR/$THISUSER/$THISREPO" ]
-then
-  RenameIfExists "$GITDIR/$THISUSER/$THISREPO"
-  echo "Adding the $THISREPO repo by $THISUSER into $GITDIR"
-  mkdir -p "$GITDIR/$THISUSER" && cd "$GITDIR/$THISUSER" && git clone https://github.com/$THISUSER/$THISREPO.git
-fi    
-
-#add all necessary repos from RepoList file
-while read line;
-do
-  GHUSER=$(echo $line | tr "/" " " | awk '{print $1}')
-  GHREPO=$(echo $line | tr "/" " " | awk '{print $2}')
-  RenameIfExists "$GITDIR/$GHUSER/$GHREPO"
-  mkdir -p "$GITDIR/$GHUSER" && cd "$GITDIR/$GHUSER" && git clone https://github.com/$GHUSER/$GHREPO.git
-done < "$scriptfolder/RepoList"
-
-#copy symlinks to $HOME
-for link in $scriptfolder/symlinks/*
-do
-  basefile="$(basename "$link")"
-  RenameIfExists "$HOME/$basefile"
-  echo "Copying $basefile to $HOME"
-  cp -d "$link" "$HOME/$basefile"
-done
+THIS_USER=qwer777
+THIS_REPO=dotfiles
+GIT_DIR="$HOME/git" #Folder all repos are installed to
+THIS_SCRIPT_FOLDER="$GIT_DIR/$THIS_USER/$THIS_REPO/setup" #Folder this script is in
+COPY_TO_HOME_FOLDER="$THIS_SCRIPT_FOLDER/COPY_TO_HOME" #Folder that contains the files to copy to $HOME
+GH_REPO_LIST_FILE="$THIS_SCRIPT_FOLDER/RepoList" #File containing the list of GitHub repos in the form of USER/REPO on each line
 
 ##Functions
+#add Github repos
+add_gh_repo () {
+gh_user=$(echo $1 | tr "/" " " | awk '{print $1}')
+gh_repo=$(echo $1 | tr "/" " " | awk '{print $2}')
+rename_if_exists "$GIT_DIR/$gh_user/$gh_repo"
+echo "Adding the $gh_repo repo by $gh_user into $GIT_DIR"
+mkdir -p "$GIT_DIR/$gh_user"
+cd "$GIT_DIR/$gh_user"
+git clone -q https://github.com/$gh_user/$gh_repo.git
+}
+
+#add all GitHub repos from $GH_REPO_LIST_FILE file
+add_gh_repos_from_file () {
+while read line;
+do
+  add_gh_repo $line
+done < "$1"
+}
+
+#copy items to $HOME
+copy_to_home () {
+for item in $COPY_TO_HOME_FOLDER/*
+do
+  basefile="$(basename "$item")"
+  rename_if_exists "$HOME/$basefile"
+  echo "Copying $basefile to $HOME"
+  cp -d "$item" "$HOME/$basefile"
+done
+}
+
+#Main function
+main() {
+add_gh_repo "$THIS_USER/$THIS_REPO"
+add_gh_repos_from_file "$GH_REPO_LIST_FILE"
+copy_to_home
+}
+ 
 #Rename anything we're looking for that exists to $name.YYYY-MM-DD_HH:MM:SS
-RenameIfExists ()
-{
+rename_if_exists () {
 if [ -e "$1" ]
 then 
   echo moving "$1" to "$1.$(date +%F_%T)"
   mv "$1" "$1.$(date +%F_%T)"
 fi  
 }
+
+main "$@"
