66a1bdd2362c76e43de6758936a5229a6fe3e63b
qwer777 <dontspammebr4h@gmail.com>
2017-03-06 09:38:05 -0600

    setup.sh pulls from testing

--- setup/setup.sh
+++ setup/setup.sh
@@ -45,7 +45,8 @@ done
 
 #Main function
 main() {
-add_gh_repo "$THIS_USER/$THIS_REPO"
+#add_gh_repo "$THIS_USER/$THIS_REPO"
+git clone -q -b testing https://github.com/$THIS_USER/$THIS_REPO.git
 add_gh_repos_from_file "$GH_REPO_LIST_FILE"
 symlink_files
 }
